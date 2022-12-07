56038bcd7069a422d2feac52cacef529440a67cf
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-12-07 11:02:17 +0000

    script quizAnimations adicioando para lidar com as animações envolvendo os eventos durante o Quiz. style.css adicionado para estilizar pontualmente alguns aspectos.

---
+++ quizAnimations.js
@@ -0,0 +1,7 @@
+const initialButton = document.querySelector('[data-initialButton="btn"]');
+const introPage = document.querySelector('.intro');
+
+
+initialButton.addEventListener('click', () => {
+  // introPage.classList.add('d-none')
+})
